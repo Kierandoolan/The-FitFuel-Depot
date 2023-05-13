72f754104504654661b15437af3ed7f4cd331c5f
Kierandoolan <kierandoolan3@gmail.com>
2023-05-13 16:04:15 +0000

    add checkout view and templates

--- the_fitfuel_depot/settings.py
+++ the_fitfuel_depot/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'the_fitfuel_depot.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -64,8 +67,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ] 
