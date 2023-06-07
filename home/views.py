362367435956d96448fa293b00d7b9df8f98f13e
Kierandoolan <kierandoolan3@gmail.com>
2023-06-07 15:02:29 +0000

    added frequently asked questions page

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def faq(request):
+    """ A view to return the frequently ask question page """
+
+    return render(request, 'home/faq.html')
