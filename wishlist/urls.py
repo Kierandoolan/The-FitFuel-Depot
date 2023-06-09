108e7ec6a11867baf19caaf70ad76a7c0818e580
Kierandoolan <kierandoolan3@gmail.com>
2023-06-09 12:47:26 +0000

    add adding and removal view and urls for favourite app

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.wishlist_view, name='wishlist'),
+    path('add_to_wishlist/<item_id>/',
+         views.add_to_wishlist, name='add_to_wishlist'),
+    path('remove_from_wishlist/<item_id>/<redirect_from>/',
+         views.remove_from_wishlist, name='remove_from_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,9 +1,28 @@
+""" wishlist Views """
 from django.shortcuts import (render, get_object_or_404, redirect)
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.urls import reverse
 from products.models import Product
-# Create your views here.
+from .models import Wishlist
+
+
+def error_403_view(request, exception):
+    '''403 error view'''
+    return render(request, '403.html', status=403)
+
+
+def error_404_view(request, exception):
+    '''404 error view'''
+    return render(request, '404.html', status=404)
+
+
+def error_500_view(request):
+    """
+    404 error view
+    """
+    return render(request, '500.html', status=500)
 
 
 @login_required
@@ -13,7 +32,7 @@ def wishlist_view(request):
     """
     wishlist_items_count = 0
     try:
-        all_wishlist = wishlist.objects.filter(username=request.user.id)[0]
+        all_wishlist = Wishlist.objects.filter(username=request.user.id)[0]
     except IndexError:
         wishlist_items = None
     else:
@@ -30,3 +49,45 @@ def wishlist_view(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def add_to_wishlist(request, item_id):
+    """
+    A view that will add a product item to wishlist
+    """
+    product = get_object_or_404(Product, pk=item_id)
+    try:
+        wishlist = get_object_or_404(Wishlist, username=request.user.id)
+    except Http404:
+        wishlist = Wishlist.objects.create(username=request.user)
+
+    if product in wishlist.products.all():
+        messages.info(request, 'The product is already in your wishlist!')
+    else:
+        wishlist.products.add(product)
+        messages.info(request, 'Added the product to your wishlist')
+
+    return redirect(reverse('product_detail', args=[item_id]))
+
+
+@login_required
+def remove_from_wishlist(request, item_id, redirect_from):
+    """
+    A view that will add a product item to wishlist
+    """
+    product = get_object_or_404(Product, pk=item_id)
+    wishlist = get_object_or_404(Wishlist, username=request.user.id)
+    if product in wishlist.products.all():
+        wishlist.products.remove(product)
+        messages.info(request, 'Removed the product '
+                               'from your wishlist list')
+    else:
+        messages.error(request, 'That product is '
+                                'not in your wishlist list!')
+    if redirect_from == 'wishlist':
+        redirect_url = reverse('wishlist')
+    else:
+        redirect_url = reverse('product_detail', args=[item_id])
+
+    return redirect(redirect_url)
