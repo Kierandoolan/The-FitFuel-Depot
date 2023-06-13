f714a4a094af869cde32e113907a2f06cd3b0966
Kierandoolan <kierandoolan3@gmail.com>
2023-06-13 11:23:38 +0000

    add customer review

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .widgets import CustomClearableFileInput
-from .models import Product, Category
+from .models import Product, Category, ReviewRating
 
 
 class ProductForm(forms.ModelForm):
@@ -19,3 +20,19 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = ReviewRating
+        fields = ['subject', 'review', 'rating']
+
+    def clean_rating(self):
+        data = self.cleaned_data['rating']
+        if data < 1:
+            raise ValidationError(('Please enter a rating between 1 and 5'))
+
+        if data > 5:
+            raise ValidationError(('Please enter a rating between 1 and 5'))
+
+        return data
--- products/views.py
+++ products/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.contrib import messages
+from django.http import Http404
+from django.views.generic import UpdateView, DeleteView
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.db.models.functions import Lower
-from .models import Product, Category
-from .forms import ProductForm
+from django.urls import reverse_lazy
+from .models import Product, Category, ReviewRating
+from .forms import ProductForm, ReviewForm
+from wishlist.models import Wishlist
+
 # Create your views here.
 
 
@@ -153,3 +160,70 @@ def delete_product(request, product_id):
     product.delete()
     messages.success(request, 'Product deleted!')
     return redirect(reverse('products'))
+
+
+@login_required
+def submit_review(request, product_id):
+    """ View to review a product """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        review_form = ReviewForm(request.POST)
+
+        if review_form.is_valid():
+            already_reviewed = ReviewRating.objects.filter(product=product,
+                                                           user=request.user)
+            if not already_reviewed:
+                ReviewRating.objects.create(
+                        product=product,
+                        user=request.user,
+                        subject=request.POST['subject'],
+                        rating=request.POST['rating'],
+                        review=request.POST['review'],
+                )
+                reviews = ReviewRating.objects.filter(product=product)
+                rating_average = get_average_rating(reviews)
+                Product.objects.filter(id=product.id).update(
+                    rating=rating_average)
+                messages.success(
+                    request, 'Your review has been successfully added!')
+            else:
+                messages.error(request, 'You have already reviewed '
+                                        'this product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+
+        messages.error(request, 'Your review has not been submitted')
+    messages.error(request, 'Invalid Method.')
+    return redirect(reverse('product_detail', args=[product.id]))
+
+
+class RedirectToPreviousMixin:
+   
+    default_redirect = '/'
+
+    def get(self, request, *args, **kwargs):
+        request.session['previous_page'] = request.META.get(
+            'HTTP_REFERER', self.default_redirect)
+        return super().get(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return self.request.session['previous_page']
+
+    
+def get_average_rating(reviews):
+    """
+    Function to get the average rating of product
+    from the reviews
+    """
+    total_reviews = 0
+    ratings_total = 0
+    rating_average = 0
+    for review in reviews:
+        total_reviews = total_reviews + 1
+        ratings_total = ratings_total + review.rating
+
+    if total_reviews > 0:
+        average_rating = (ratings_total / total_reviews)
+        rating_average = round(average_rating, 1)
+        return rating_average
+    else:
+        return rating_average
