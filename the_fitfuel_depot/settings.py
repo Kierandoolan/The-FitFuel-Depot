5554c76991303f5fe188c46d0de5b5fe98d7d660
Kierandoolan <kierandoolan3@gmail.com>
2023-06-13 15:35:05 +0000

    remove secret key and set debug

--- the_fitfuel_depot/settings.py
+++ the_fitfuel_depot/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=&mb#a$9^!ft_#1@e$tj65(e=o%*at$)6%9hd$(n@55t9jkuxp'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fitfuel-depot.herokuapp.com', 'localhost']
 
