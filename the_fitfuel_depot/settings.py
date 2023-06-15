71bf0def311a87ea01746fce7126b4887432f64b
Kierandoolan <kierandoolan3@gmail.com>
2023-06-15 17:41:12 +0000

    fix product management error

--- the_fitfuel_depot/settings.py
+++ the_fitfuel_depot/settings.py
@@ -11,13 +11,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'wffwe')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['fitfuel-depot.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['fitfuel-depot.herokuapp.com', 'localhost',]
 
 
 # Application definition
